06fedfc7a5050fa47ffed1a11dcc2ae2c412e7a7
roy-ermers <dangergamesnl@gmail.com>
2018-06-19 23:19:44 +0200

    start of webcomponents

--- MojoEvents/wwwroot/js/site.js
+++ MojoEvents/wwwroot/js/site.js
@@ -1,1 +1,33 @@
-﻿// Write your Javascript code.
+﻿class MojoCarrousel extends HTMLElement {
+	Roll(panels) {
+		this.currentIndex++;
+		if (this.currentIndex > panels.length - 1) this.currentIndex = 0;
+		panels.forEach((e, i) => {
+			e.style.opacity = i == this.currentIndex ? "1" : "0";
+		});
+	}
+	constructor() {
+		super();
+		let shadow = this.attachShadow({ mode: "open" });
+		shadow.innerHTML = "<slot></slot>";
+		let style = document.createElement("style");
+		style.textContent = ":host { display: block; height: 25vh; } slot { position: absolute; transition: opacity 250ms ease-in-out; }";
+		shadow.appendChild(style);
+	}
+	connectedCallback() {
+		this.Panels = Array.from(this.children);
+		let timer;
+		if (this.hasAttribute("interval"))
+			timer = this.getAttribute("interval");
+
+		else this.timer = 1000;
+		this.currentIndex = 0;
+		window.setInterval(() => this.Roll(this.Panels), timer);
+		this.Roll(this.Panels);
+	}
+	disconnectedCallback() {
+		window.clearInterval(() => this.Roll(this.Panels));
+	}
+
+}
+customElements.define("mojo-carrousel", MojoCarrousel);
