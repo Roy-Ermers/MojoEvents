d0aae13d3e1b2c7abd91e21a62a8c189b8d2a438
roy-ermers <dangergamesnl@gmail.com>
2018-06-27 11:30:55 +0200

    Fixed some styling issues and added logout possibility

--- MojoEvents/Pages/Users.cshtml.cs
+++ MojoEvents/Pages/Users.cshtml.cs
@@ -12,18 +12,23 @@ namespace MojoEvents.Pages
 {
     public class UsersModel : PageModel
     {
-        public List<User> Users { get; private set; }
+        public string Message { get; private set; }
+        public List<User> GetUsers()
+        {
+            List<User> Users = new List<User>();
+            
+            if (Request.HttpContext.Session.GetInt32("UserID") == 0)
+                Users = Models.User.Read();
+            else
+                Users = Models.User.Read().FindAll(x => x.OwnerID == Request.HttpContext.Session.GetInt32("UserID"));
+            
+            return Users;
+        }
+
         public IActionResult OnGet()
         {
             if (!HttpContext.Session.GetInt32("UserID").HasValue)
-                return Redirect("./login?referer=analytics");
-            else
-            {
-                if (Request.HttpContext.Session.GetInt32("UserID") == 0)
-                    Users = Models.User.Read();
-                else
-                    Users = Models.User.Read().FindAll(x => x.OwnerID == Request.HttpContext.Session.GetInt32("UserID"));
-            }
+                return Redirect("./login?referer=users");
             return Page();
         }
     }
